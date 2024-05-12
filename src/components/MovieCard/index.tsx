a304fa97c928603b196bed3a548620ef77dbba85
Aliaksei Sazonau <sazonleha010203@gmail.com>
2024-05-13 00:49:33 +0300

    start movieCard

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,3 +1,42 @@
-export const MovieCard = () => {
-    
-}
+import {
+  ActionIcon,
+  Box,
+  Flex,
+  Group,
+  Image,
+  Paper,
+  Text,
+} from "@mantine/core";
+import { Movie } from "../../pages/Home";
+
+export const MovieCard = ({ data }: { data: Movie }) => {
+  return (
+    <Paper p={24}>
+      <Flex>
+        <Image
+          w={120}
+          src={
+            data.poster_path
+              ? "https://image.tmdb.org/t/p/original" + data.poster_path
+              : "/no-poster.jpg"
+          }
+          alt={data.title}
+        />
+        <Box ml={16} w="100%">
+          <Group justify={"space-between"} align="flex-start">
+            <Text w="80%" size={"20px"} lh={1.3} c="purple.5" fw={600}>
+              {data.title}
+            </Text>
+            <ActionIcon variant="transparent" size="lg">
+              <Image w={28} h={28} src="/star.svg" alt="rating icon" />
+            </ActionIcon>
+          </Group>
+          {data.release_date && (
+            <Text>{new Date(data.release_date).getFullYear()}</Text>
+          )}
+          {data.vote_count > 0 && <Text>{data.vote_average}, {data.vote_count}</Text>}
+        </Box>
+      </Flex>
+    </Paper>
+  );
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,11 +8,16 @@ import {
   Grid,
   Image,
   NumberInput,
+  Loader,
+  Center,
+  ComboboxProps,
 } from "@mantine/core";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
-import Down from "/down.svg";
+import DownSvg from "/down.svg";
+import CloseSvg from "/close.svg";
+import { MovieCard } from "../../components/MovieCard";
 
 // import style from "./Home.module.scss";
 
@@ -20,6 +25,31 @@ interface Genre {
   id: number;
   name: string;
 }
+
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface DataMovies {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
 const years: string[] = Array.from({ length: 2030 - 1870 + 1 }, (n, i) =>
   String(i + 1870)
 ).reverse();
@@ -32,10 +62,16 @@ const sortValues: string[] = [
   "Least Voted",
 ];
 
+// ComboboxLikeProps.comboboxProps?: ComboboxProps | undefined
+
+const dropdownProps: ComboboxProps | undefined = {
+  transitionProps: { transition: "pop", duration: 300 }
+};
+
 const fetchGenres = (): Promise<Genre[]> =>
   axios.get("/api/genres").then((response) => response.data.genres);
 
-const fetchMovies = (): Promise<any> =>
+const fetchMovies = (): Promise<DataMovies> =>
   axios.get("/api/movies").then((response) => response.data);
 
 export const Home = () => {
@@ -56,6 +92,7 @@ export const Home = () => {
   });
 
   const {
+    isLoading: isLoadingMovies,
     isError: isErrorMovies,
     error: errorMovies,
     data: dataMovies,
@@ -70,7 +107,8 @@ export const Home = () => {
     console.log(errorGenres.message);
   }
 
-  const iconDown = <Image src={Down} alt="icon down" h={24} w={24} />;
+  const iconDown = <Image src={DownSvg} alt="icon down" h={24} w={24} />;
+  const iconClose = <Image src={CloseSvg} alt="icon close" h={16} w={16} />;
 
   const resetFilters = (): void => {
     setGenres([]);
@@ -98,13 +136,11 @@ export const Home = () => {
       <Text size="32px" fw={700} pb={40}>
         Movies
       </Text>
-      <Grid align="flex-end" gutter="16px" columns={10}>
+      <Grid align="flex-end" gutter="16px" columns={10.13}>
         <Grid.Col span={{ base: 10, md: 5, lg: 3 }}>
           <MultiSelect
             size="md"
-            comboboxProps={{
-              transitionProps: { transition: "pop", duration: 200 },
-            }}
+            comboboxProps={dropdownProps}
             rightSection={iconDown}
             label="Genres"
             value={genres}
@@ -119,9 +155,7 @@ export const Home = () => {
         <Grid.Col span={{ base: 10, md: 5, lg: 3 }}>
           <Select
             size="md"
-            comboboxProps={{
-              transitionProps: { transition: "pop", duration: 200 },
-            }}
+            comboboxProps={dropdownProps}
             rightSection={iconDown}
             label="Release year"
             value={year}
@@ -158,16 +192,17 @@ export const Home = () => {
           </Grid>
         </Grid.Col>
         <Grid.Col span="content">
-          <UnstyledButton {...resetBtnProps}>Reset filters</UnstyledButton>
+          <UnstyledButton {...resetBtnProps}>
+            <span>Reset filters</span>
+            {iconClose}
+          </UnstyledButton>
         </Grid.Col>
       </Grid>
       <Grid align="flex-end" justify="end" gutter="16px" columns={10} my={24}>
         <Grid.Col span={{ base: 10, md: 5, lg: 3 }}>
           <Select
             size="md"
-            comboboxProps={{
-              transitionProps: { transition: "pop", duration: 200 },
-            }}
+            comboboxProps={dropdownProps}
             rightSection={iconDown}
             label="Sort by"
             value={sort}
@@ -177,6 +212,18 @@ export const Home = () => {
             data={sortValues}
           />
         </Grid.Col>
+      </Grid>
+      <Grid>
+        {isLoadingMovies && (
+          <Center w="100%" h="40vh" component={Loader} color="purple.4" />
+        )}
+        {dataMovies?.results.map((elem) => {
+          return (
+            <Grid.Col span={{ base: 6 }}>
+              <MovieCard data={elem} />
+            </Grid.Col>
+          );
+        })}
       </Grid>
     </Container>
   );
